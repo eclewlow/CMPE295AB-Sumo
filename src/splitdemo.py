605612e68da0858cab7da58d06679c01e25c12d6
Eugene Clewlow <eclewlow@Eugenes-MacBook-Pro.local>
2022-10-21 11:58:46 -0700

    added platoon collective lane check before change

--- src/splitdemo.py
+++ src/splitdemo.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2017 Michele Segata <segata@ccs-labs.org>
+# Copyright (c) 2022 Abhishek Bharambe <abhishek.bharambe@sjsu.edu>
+# Copyright (c) 2022 Eugene Clewlow <eugene.clewlow@sjsu.edu>
+# Copyright (c) 2022 Kanak Kshirsagar <kanak.kshirsagar@sjsu.edu>
+# Copyright (c) 2022 Spoorthi Devanand <spoorthi.devanand@sjsu.edu>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -19,6 +22,8 @@
 import os
 import sys
 import time
+import matplotlib.pyplot as plt
+from collections import defaultdict
 
 import ccparams as cc
 import random
@@ -62,12 +67,16 @@ FRONT_JOIN = "v.%d" % (JOIN_POSITION - 1)
 BEHIND_JOIN = "v.%d" % JOIN_POSITION
 N_VEHICLES = 6
 JOINER = "v.%d" % N_VEHICLES
-LEFT_SLOW_VEHICLE = "v.%d" % (N_VEHICLES+1)
-RIGHT_SLOW_VEHICLE = "v.%d" % (N_VEHICLES+2)
+LEFT_SLOW_VEHICLE = "v.%d" % (N_VEHICLES + 1)
+RIGHT_SLOW_VEHICLE = "v.%d" % (N_VEHICLES + 2)
 
 # sumo launch command
 sumoBinary = sumolib.checkBinary('sumo-gui')
 sumoCmd = [sumoBinary, "-D", "-c", "cfg/freeway_test.sumocfg"]
+
+x = defaultdict(list)
+y_accel = defaultdict(list)
+y_vel = defaultdict(list)
 
 
 def add_vehicles(n, real_engine=False):
@@ -89,7 +98,7 @@ def add_vehicles(n, real_engine=False):
 
     # slow vehicle Lane 0
     vid = "v.%d" % n
-    add_vehicle(vid, (n + 10) * (DISTANCE + LENGTH) + 50, 0, SPEED - 20, DISTANCE, real_engine, type_id='PlatoonCar')
+    add_vehicle(vid, (n + 9) * (DISTANCE + LENGTH) + 50, 0, SPEED - 20, DISTANCE, real_engine, type_id='PlatoonCar')
     time.sleep(1)
     change_lane(vid, 0)
     set_par(vid, cc.PAR_ACTIVE_CONTROLLER, cc.ACC)
@@ -121,20 +130,20 @@ def open_gap(vid, jid, topology, n):
     set_par(vid, cc.PAR_ACTIVE_CONTROLLER, cc.FAKED_CACC)
     return topology
 
+
 def set_leader(vid, lid, topology, n):
     index = int(vid.split(".")[1])
     for i in range(index, n):
-        # temporarily change the leader
         topology["v.%d" % i]["leader"] = vid
-    # the front vehicle if the vehicle opening the gap is the joiner
     topology[vid]["front"] = lid
     set_par(vid, cc.PAR_ACTIVE_CONTROLLER, cc.FAKED_CACC)
 
     return topology
 
+
 def front_speed_up(topology):
     topology.pop(LEADER)
-    set_par(LEADER, cc.PAR_CC_DESIRED_SPEED, SPEED-15)
+    set_par(LEADER, cc.PAR_CC_DESIRED_SPEED, SPEED - 15)
     set_par(LEADER, cc.PAR_ACTIVE_CONTROLLER, cc.ACC)
 
     return topology
@@ -149,13 +158,33 @@ def reset_leader(vid, topology, n):
     return topology
 
 
+def can_change_lane(leader_id, direction, n):
+    result = True
+    index = int(leader_id.split(".")[1])
+    for i in range(index, n):
+        vid = "v.%d" % i
+        result = result and traci.vehicle.couldChangeLane(vid, direction)
+    return result
+
+
+def record_data_points():
+    for i in range(N_VEHICLES):
+        vid = "v.%d" % i
+        vehicle_data = get_par(vid, cc.PAR_SPEED_AND_ACCELERATION)
+        (vel, acc, u, xpos, ypos, t, _, _, _) = cc.unpack(vehicle_data)
+
+        x[vid].append(t)
+        y_accel[vid].append(acc)
+        y_vel[vid].append(vel)
+
+
 def main(demo_mode, real_engine, setter=None):
     # used to randomly color the vehicles
     random.seed(1)
     start_sumo("cfg/freeway_test.sumocfg", False)
     step = 0
     state = FIRST_STATE
-    while running(demo_mode, step, 6000):
+    while running(demo_mode, step, 4000):
 
         # when reaching 60 seconds, reset the simulation when in demo_mode
         if demo_mode and step == 6000:
@@ -172,6 +201,7 @@ def main(demo_mode, real_engine, setter=None):
             traci.gui.setZoom("View #0", 20000)
         if step % 10 == 1:
             communicate(topology)
+            record_data_points()
         if step == 100:
             state = GOING_TO_POSITION
             topology = get_in_position('v.0', LEFT_SLOW_VEHICLE, topology)
@@ -180,17 +210,17 @@ def main(demo_mode, real_engine, setter=None):
                 state = FRONT_CHANGING_LANES
                 topology = open_gap(BEHIND_JOIN, FRONT_JOIN, topology, N_VEHICLES)
         if state == FRONT_CHANGING_LANES:
-            for i in range(JOIN_POSITION):
-                vid = f'v.{i}'
-                change_lane(vid, 0)
-            state = FRONT_SPEED_UP
-
-            topology = set_leader(BEHIND_JOIN, LEFT_SLOW_VEHICLE, topology, N_VEHICLES)
-
-            topology = front_speed_up(topology)
+            if can_change_lane(LEADER, -1, JOIN_POSITION):
+                for i in range(JOIN_POSITION):
+                    vid = f'v.{i}'
+                    change_lane(vid, 0)
+                state = FRONT_SPEED_UP
+                topology = set_leader(BEHIND_JOIN, LEFT_SLOW_VEHICLE, topology, N_VEHICLES)
+                topology = front_speed_up(topology)
 
         if state == FRONT_SPEED_UP:
-            if get_distance(BEHIND_JOIN, LEFT_SLOW_VEHICLE) < JOIN_DISTANCE + 1 and traci.vehicle.couldChangeLane(BEHIND_JOIN, -1):
+            if get_distance(BEHIND_JOIN, LEFT_SLOW_VEHICLE) < JOIN_DISTANCE + 1 and can_change_lane(BEHIND_JOIN, -1,
+                                                                                                    N_VEHICLES):
                 state = REAR_CHANGING_LANES
         if state == REAR_CHANGING_LANES:
             for i in range(JOIN_POSITION, N_VEHICLES):
@@ -199,12 +229,31 @@ def main(demo_mode, real_engine, setter=None):
             topology = reset_leader(BEHIND_JOIN, topology, N_VEHICLES)
             state = COMPLETED
 
-
-
         step += 1
 
     traci.close()
 
 
 if __name__ == "__main__":
-    main(True, False)
+    main(False, False)
+    plt.rcParams["figure.figsize"] = [7.00, 3.50]
+    plt.rcParams["figure.autolayout"] = True
+    plt.figure(0)
+    for i in range(N_VEHICLES):
+        vid = "v.%d" % i
+        plt.plot(x[vid], y_accel[vid], label=f"Platoon Vehicle {i + 1}", linestyle="-")
+    plt.title("Acceleration of Platoon Vehicles in Platoon Split Scenario")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Acceleration (m/s^2)")
+    plt.legend()
+    plt.show()
+
+    plt.figure(1)
+    for i in range(N_VEHICLES):
+        vid = "v.%d" % i
+        plt.plot(x[vid], y_vel[vid], label=f"Platoon Vehicle {i + 1}", linestyle="-")
+    plt.title("Speed of Platoon Vehicles in Platoon Split Scenario")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Velocity (m/s)")
+    plt.legend()
+    plt.show()
